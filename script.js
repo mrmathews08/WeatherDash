77f1deea7d118cd67cf8516b4ecd29f0cd109338
Tyler Mathews <mrmathews08@gmail.com>
2020-08-02 16:11:25 -0600

    consolelogging

--- script.js
+++ script.js
@@ -1,7 +1,50 @@
-// store the value of the input 
-var city = $("#searchTerm").val();
+// store the value of the input
+let city = $("#searchTerm").val();
+// store api key
+const apiKey = "&appid=afaa8eea1769b4359fd8e07b2efcefbd";
 
-// my API key 
-var apiKey = "ff35f00d6fe8a98f298ad9e46422f88b";
+let date = new Date();
 
-var date = new Date();
+$("#searchTerm").keypress(function(event) { 
+	
+	if (event.keyCode === 13) { 
+		event.preventDefault();
+		$("#searchBtn").click(); 
+	} 
+});
+
+$("#searchBtn").on("click", function() {
+
+  $('#forecastH5').addClass('show');
+
+  // get the value of the input from user
+  city = $("#searchTerm").val();
+  
+  // clear input box
+  $("#searchTerm").val("");  
+
+  // full url to call api
+  const queryUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + city + apiKey;
+
+  $.ajax({
+    url: queryUrl,
+    method: "GET"
+  })
+  .then(function (response){
+
+    console.log(response)
+
+    console.log(response.name)
+    console.log(response.weather[0].icon)
+
+    let tempF = (response.main.temp - 273.15) * 1.80 + 32;
+    console.log(Math.floor(tempF))
+
+    console.log(response.main.humidity)
+
+    console.log(response.wind.speed)
+
+    
+
+    })
+  });
